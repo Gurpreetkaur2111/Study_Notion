797733ae97dc3445a84623d38beb18347a84beec
gurpreet <gurpkaur37@gmail.com>
2025-06-27 19:43:42 +0530

    Added components folder and updated Home layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
-        <Route path="/" element={<Hom/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </div>
   );
