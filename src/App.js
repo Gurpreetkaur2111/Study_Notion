b608d8740ea8596e43d991696e4b2375ac5f0598
gurpreet <gurpkaur37@gmail.com>
2025-07-05 17:58:32 +0530

    Add About page and ContactUsForm, update App.js and dependencies

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import About from "./pages/About"
 
 function App() {
   return (
@@ -29,6 +30,15 @@ function App() {
             </OpenRoute>
           }
         />
+
+    <Route 
+      path="about"
+      element={
+        <OpenRoute>
+          <About/>
+        </OpenRoute>
+      }
+    /> 
     </Routes>
 
    </div>
